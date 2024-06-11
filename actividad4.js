e601d395d22415b275b320502ac59f9afff5ee57
Mauricio <mauriciofloresc6@gmail.com>
2024-06-11 16:03:37 -0600

    Tercer commit

--- actividad4.js
+++ actividad4.js
@@ -1,9 +1,7 @@
 function num(n){
 for(let i = 1; i <= 2*n-1; i++){
-console.log((Math.abs((n-1))+1).toString().repeat(n));
+console.log((Math.abs((n-i))+1).toString().repeat(n));
 }
 } 
-console.log();
-num(3);
 console.log();
 num(5);
